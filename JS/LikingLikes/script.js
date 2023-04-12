af150757a5386126b60706e86900831de6330883
Olson A Pereira <46116497+OlsonPereira@users.noreply.github.com>
2023-04-12 12:37:51 -0400

    Add files via upload

---
+++ JS/LikingLikes/script.js
@@ -0,0 +1,17 @@
+function likea() {
+    likenum = document.querySelector("#like-a span");
+    var likes = parseInt(likenum.textContent);
+    likenum.textContent = likes + 1;
+}
+
+function likeb() {
+    likenum = document.querySelector("#like-b span");
+    var likes = parseInt(likenum.textContent);
+    likenum.textContent = likes + 1;
+}
+
+function likec() {
+    likenum = document.querySelector("#like-c span");
+    var likes = parseInt(likenum.textContent);
+    likenum.textContent = likes + 1;
+}
